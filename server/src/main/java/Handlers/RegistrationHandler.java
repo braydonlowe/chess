fb073e2957518c956879fa8640562fd763f26553
Braydon Lowe <braydonslowe17@gmail.com>
2024-05-27 23:29:08 -0600

    Errors really fixed now

--- server/src/main/java/Handlers/RegistrationHandler.java
+++ server/src/main/java/Handlers/RegistrationHandler.java
@@ -10,6 +10,6 @@ public class RegistrationHandler {
     public static Object registerUser(Request req, Response res) {
         User user = JsonUtil.fromJson(req.body(), User.class);
         //We are returning a line from the auth table.
-
+        return null;
     }
 }

a531be7b0c94287cd7abb98dbaf5943d1693164c
Braydon Lowe <braydonslowe17@gmail.com>
2024-05-27 20:00:51 -0600

    User data access cleared implemented

--- server/src/main/java/Handlers/ClearHandler.java
+++ server/src/main/java/Handlers/ClearHandler.java
@@ -3,10 +3,20 @@ package Handlers;
 //Imports
 import spark.Request;
 import spark.Response;
+import java.util.Map;
+import dataaccess.DAO.UserDataAccess;
+import server.JsonUtil;
 
 public class ClearHandler {
+
     public static Object handleClear(Request req, Response res) {
-        //Stuff to handle the clear
-        return null;
+        //Implement the clearing of the database.
+        UserDataAccess.clearUserData();
+
+
+
+        res.status(200);
+
+        return JsonUtil.toJson(Map.of("message", "Database cleared"));
     }
 }
--- server/src/main/java/dataaccess/DAO/UserDataAccess.java
+++ server/src/main/java/dataaccess/DAO/UserDataAccess.java
@@ -1,4 +1,15 @@
 package dataaccess.DAO;
 
+
+//Imports
+import java.util.Map;
+import java.util.HashMap;
+import Model.User;
+
 public class UserDataAccess {
+    private static final Map<String, User> userTable = new HashMap<>();
+
+    public static void clearUserData() {
+        userTable.clear();
+    }
 }

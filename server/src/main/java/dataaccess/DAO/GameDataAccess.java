adc0850684e4c1d8ecaf15a79c3027d092b4c9e2
Braydon Lowe <braydonslowe17@gmail.com>
2024-05-27 20:44:10 -0600

    Game DAO added

--- server/src/main/java/dataaccess/DAO/GameDataAccess.java
+++ server/src/main/java/dataaccess/DAO/GameDataAccess.java
@@ -1,14 +1,35 @@
 package dataaccess.DAO;
 
-import Model.User;
+import Model.Game;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class GameDataAccess {
-    private static final Map<String, User> gameTable = new HashMap<>();
+    private static final Map<String, Game> gameTable = new HashMap<>();
 
     public static void clearGameData() {
         gameTable.clear();
     }
+
+    //Create
+    public static void createGame(String ID, Game game) {
+        gameTable.put(ID, game);
+    }
+
+    //Read
+    public static Game readGame(String ID) {
+        return gameTable.get(ID);
+    }
+
+    //Update
+    public static void updateGame(String ID, Game game) {
+        createGame(ID, game);
+    }
+
+    //Delete
+    public static void deleteGame(String ID) {
+        gameTable.remove(ID);
+    }
+
 }

76ba44e895b31b0a30df259ead701f7e6b175342
Braydon Lowe <braydonslowe17@gmail.com>
2024-05-28 23:01:54 -0600

    passing all tests

--- server/src/test/java/service/MyJavaTest.java
+++ server/src/test/java/service/MyJavaTest.java
@@ -12,38 +12,40 @@ import dataaccess.DAO.AuthDataAccess;
 import dataaccess.DAO.GameDataAccess;
 import dataaccess.DAO.UserDataAccess;
 import dataaccess.DataAccessException;
-import org.eclipse.jetty.util.log.Log;
 import org.junit.jupiter.api.*;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyJavaTest {
-    private AuthDataAccess bananaGrahms;
-    private GameDataAccess juniorbaconCheese;
+    private AuthDataAccess authData;
+    private GameDataAccess gameData;
     private UserDataAccess users;
 
     @BeforeEach
     void dataAccessReset() {
-        bananaGrahms = new AuthDataAccess();
-        juniorbaconCheese = new GameDataAccess();
+        authData = new AuthDataAccess();
+        gameData = new GameDataAccess();
         users = new UserDataAccess();
+        authData.clear();
+        gameData.clear();
+        users.clear();
     }
+
+
 
     //Clear Database
     @Test
     void clearTest() throws DataAccessException {
-        bananaGrahms.create(bananaGrahms.createAuth(), new Auth(bananaGrahms.createAuth(), "GoodUserName"));
-        juniorbaconCheese.create(bananaGrahms.createAuth(), new Game(bananaGrahms.createAuth(), "yourMom", "myMom", "TheEnd", new ChessGame()));
+        authData.create(authData.createAuth(), new Auth(authData.createAuth(), "GoodUserName"));
+        gameData.create(authData.createAuth(), new Game(authData.createAuth(), "yourMom", "myMom", "TheEnd", new ChessGame()));
         users.create("yourMom", new User("yourMom", "1", "*"));
         users.create("myMom", new User("myMom", "2", "*"));
 
-        ClearService sirCleansAlot = new ClearService(bananaGrahms, juniorbaconCheese, users);
+        ClearService sirCleansAlot = new ClearService(authData, gameData, users);
         sirCleansAlot.clearData();
 
-        assertEquals(0, bananaGrahms.size());
-        assertEquals(0, juniorbaconCheese.size());
+        assertEquals(0, authData.size());
+        assertEquals(0, gameData.size());
         assertEquals(0, users.size());
 
     }
@@ -53,25 +55,27 @@ public class MyJavaTest {
 
     @Test
     void createServiceTestPos() throws DataAccessException {
-        assertEquals(0, juniorbaconCheese.size());
-        String auth = bananaGrahms.createAuth();
+        assertEquals(0, gameData.size());
+        String auth = authData.createAuth();
+        authData.create(auth, new Auth(auth, "GoodUserName"));
+
         String gameName = "TheEnd";
 
-        CreateGameService gameServ = new CreateGameService(bananaGrahms, juniorbaconCheese, users);
+        CreateGameService gameServ = new CreateGameService(authData, gameData, users);
         gameServ.createGame(auth, gameName);
 
-        assertEquals(1, juniorbaconCheese.size());
+        assertEquals(1, gameData.size());
 
     }
 
     @Test
     void createServiceTestNeg() throws DataAccessException {
-        assertEquals(0, juniorbaconCheese.size());
+        assertEquals(0, gameData.size());
         String auth = null;
         String gameName = "TheEnd";
 
         assertThrows(DataAccessException.class, () -> {
-            CreateGameService gameServ = new CreateGameService(bananaGrahms, juniorbaconCheese, users);
+            CreateGameService gameServ = new CreateGameService(authData, gameData, users);
             gameServ.createGame(auth, gameName);
         });
 
@@ -81,30 +85,30 @@ public class MyJavaTest {
     @Test
     void joinGameServicePos() throws DataAccessException {
         users.create("GoodUserName", new User("GoodUserName", "1", "*"));
-        String auth = bananaGrahms.createAuth();
-        bananaGrahms.create(auth, new Auth(auth, "GoodUserName"));
+        String auth = authData.createAuth();
+        authData.create(auth, new Auth(auth, "GoodUserName"));
         String gameName = "TheEnd";
-        CreateGameService gameServ = new CreateGameService(bananaGrahms, juniorbaconCheese, users);
+        CreateGameService gameServ = new CreateGameService(authData, gameData, users);
         CreateGameRecord gameID = gameServ.createGame(auth, gameName);
-        JoinGameService joinServ = new JoinGameService(bananaGrahms, juniorbaconCheese, users);
+        JoinGameService joinServ = new JoinGameService(authData, gameData, users);
         joinServ.joinGame(auth, gameID.gameID(), "WHITE");
 
-        assertEquals("GoodUserName", juniorbaconCheese.read(gameID.gameID()).whiteUsername());
+        assertEquals("GoodUserName", gameData.read(gameID.gameID()).whiteUsername());
     }
 
     @Test
     void joinGameServiceNeg() throws DataAccessException {
         users.create("GoodUserName", new User("GoodUserName", "1", "*"));
-        String auth = bananaGrahms.createAuth();
-        bananaGrahms.create(auth, new Auth(auth, "GoodUserName"));
+        String auth = authData.createAuth();
+        authData.create(auth, new Auth(auth, "GoodUserName"));
 
         users.create("BadUserName", new User("BadUserName", "1", "*"));
-        String badAuth = bananaGrahms.createAuth();
+        String badAuth = authData.createAuth();
 
         String gameName = "TheEnd";
-        CreateGameService gameServ = new CreateGameService(bananaGrahms, juniorbaconCheese, users);
+        CreateGameService gameServ = new CreateGameService(authData, gameData, users);
         CreateGameRecord gameID = gameServ.createGame(auth, gameName);
-        JoinGameService joinServ = new JoinGameService(bananaGrahms, juniorbaconCheese, users);
+        JoinGameService joinServ = new JoinGameService(authData, gameData, users);
 
         joinServ.joinGame(auth, gameID.gameID(), "WHITE");
 
@@ -116,10 +120,10 @@ public class MyJavaTest {
     @Test
     void listGameServiceTestPos() throws DataAccessException {
         users.create("GoodUserName", new User("GoodUserName", "1", "*"));
-        String auth = bananaGrahms.createAuth();
-        bananaGrahms.create(auth, new Auth(auth, "GoodUserName"));
+        String auth = authData.createAuth();
+        authData.create(auth, new Auth(auth, "GoodUserName"));
         users.create("BadUserName", new User("BadUserName", "1", "*"));
-        ListGameService listGame = new ListGameService(bananaGrahms, juniorbaconCheese, users);
+        ListGameService listGame = new ListGameService(authData, gameData, users);
         ListOfGamesRecord games = listGame.listGame(auth);
         assertNotNull(games);
     }
@@ -127,10 +131,10 @@ public class MyJavaTest {
     @Test
     void listGamesServiceNeg() throws DataAccessException{
         users.create("GoodUserName", new User("GoodUserName", "1", "*"));
-        String auth = bananaGrahms.createAuth();
-        bananaGrahms.create(auth, new Auth(auth, "GoodUserName"));
+        String auth = authData.createAuth();
+        authData.create(auth, new Auth(auth, "GoodUserName"));
         users.create("BadUserName", new User("BadUserName", "1", "*"));
-        ListGameService listGame = new ListGameService(bananaGrahms, juniorbaconCheese, users);
+        ListGameService listGame = new ListGameService(authData, gameData, users);
         assertThrows(DataAccessException.class, () -> {
             listGame.listGame(null);
         });
@@ -141,11 +145,11 @@ public class MyJavaTest {
     @Test
     void loginServicePos() throws DataAccessException {
         users.create("GoodUserName", new User("GoodUserName", "1", "*"));
-        String auth = bananaGrahms.createAuth();
-        bananaGrahms.create(auth, new Auth(auth, "GoodUserName"));
+        String auth = authData.createAuth();
+        authData.create(auth, new Auth(auth, "GoodUserName"));
         User currentUser = users.read("GoodUserName");
 
-        LoginService loginServ = new LoginService(bananaGrahms, juniorbaconCheese, users);
+        LoginService loginServ = new LoginService(authData, gameData, users);
 
         assertNotEquals(auth, loginServ.loginUser(currentUser));
     }
@@ -153,10 +157,10 @@ public class MyJavaTest {
     @Test
     void loginServiceNeg() throws DataAccessException {
         users.create("GoodUserName", new User("GoodUserName", "1", "*"));
-        String auth = bananaGrahms.createAuth();
-        bananaGrahms.create(auth, new Auth(auth, "GoodUserName"));
+        String auth = authData.createAuth();
+        authData.create(auth, new Auth(auth, "GoodUserName"));
 
-        LoginService loginServ = new LoginService(bananaGrahms, juniorbaconCheese, users);
+        LoginService loginServ = new LoginService(authData, gameData, users);
         assertThrows(NullPointerException.class, () -> {
             loginServ.loginUser(null);
         });
@@ -165,21 +169,21 @@ public class MyJavaTest {
 
     @Test
     void logoutTest() throws DataAccessException {
-        String auth = bananaGrahms.createAuth();
-        bananaGrahms.create(auth, new Auth(auth, "GoodUserName"));
+        String auth = authData.createAuth();
+        authData.create(auth, new Auth(auth, "GoodUserName"));
 
-        LogoutService logoutServ = new LogoutService(bananaGrahms, juniorbaconCheese, users);
+        LogoutService logoutServ = new LogoutService(authData, gameData, users);
         logoutServ.logoutUser(auth);
 
-        assertEquals(0, bananaGrahms.size());
+        assertEquals(0, authData.size());
     }
 
     @Test
     void logoutTestNeg() throws DataAccessException {
-        String auth = bananaGrahms.createAuth();
-        bananaGrahms.create(auth, new Auth(auth, "GoodUserName"));
+        String auth = authData.createAuth();
+        authData.create(auth, new Auth(auth, "GoodUserName"));
 
-        LogoutService logoutServ = new LogoutService(bananaGrahms, juniorbaconCheese, users);
+        LogoutService logoutServ = new LogoutService(authData, gameData, users);
 
         assertThrows(DataAccessException.class, () -> {
             logoutServ.logoutUser(null);
@@ -188,7 +192,7 @@ public class MyJavaTest {
 
     @Test
     void registerTestPos() throws DataAccessException{
-        RegistrationService regServ = new RegistrationService(bananaGrahms, juniorbaconCheese, users);
+        RegistrationService regServ = new RegistrationService(authData, gameData, users);
         User newUser = new User("GoodUserName", "1", "*");
         regServ.createUser(newUser);
         assertEquals(1, users.size());
@@ -196,7 +200,7 @@ public class MyJavaTest {
 
     @Test
     void registerTestNeg() throws DataAccessException {
-        RegistrationService regServ = new RegistrationService(bananaGrahms, juniorbaconCheese, users);
+        RegistrationService regServ = new RegistrationService(authData, gameData, users);
         users.create("GoodUserName", new User("GoodUserName", "1", "*"));
         User user = users.read("GoodUserName");
         assertThrows(DataAccessException.class, () -> {

3f6ad3d2ae485ab291e3d6bd20fea34d22db1312
Braydon Lowe <braydonslowe17@gmail.com>
2024-06-10 15:33:58 -0600

    Server facade utils mostly done

--- client/src/main/java/ui/ServerFacadeUtils.java
+++ client/src/main/java/ui/ServerFacadeUtils.java
@@ -13,13 +13,8 @@ import java.net.URL;
 
 public class ServerFacadeUtils {
 
-    //Write request
-    public static void writeRequest() {
-
-    }
-
     //Copied and modified from petshop
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass, String serverUrl) throws Exception {
+    public static <T> T makeRequest(String method, String path, Object request, Class<T> responseClass, String serverUrl) throws Exception {
         try {
             URL url = (new URI(serverUrl + path)).toURL();
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
@@ -36,7 +31,7 @@ public class ServerFacadeUtils {
         }
     }
 
-    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+    public static void writeBody(Object request, HttpURLConnection http) throws IOException {
         if (request != null) {
             http.addRequestProperty("Content-Type", "application/json");
             String reqData = new Gson().toJson(request);
@@ -47,14 +42,14 @@ public class ServerFacadeUtils {
     }
 
 
-    private void throwIfNotSuccessful(HttpURLConnection http) throws Exception {
+    private static void throwIfNotSuccessful(HttpURLConnection http) throws Exception {
         var status = http.getResponseCode();
         if (!isSuccessful(status)) {
             throw new Exception("failure: " + status);
         }
     }
 
-    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+    public static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
         T response = null;
         if (http.getContentLength() < 0) {
             try (InputStream respBody = http.getInputStream()) {
@@ -67,7 +62,7 @@ public class ServerFacadeUtils {
         return response;
     }
 
-    private boolean isSuccessful(int status) {
+    private static boolean isSuccessful(int status) {
         if (status == 200) {
             return true;
         }
